276af7fa6d13c27c5746c2e4ccd44d376761c70c
mikaelgba <mikaelgba@gmail.com>
2021-04-20 13:21:15 -0300

    add api gereric / add Authentication with tokenn

--- Django/drf/project/app/urls.py
+++ Django/drf/project/app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HQ_details, HQ_list
+from .views import HQ_details, HQ_list, GenericApiView
 
 urlpatterns = [
     path('hqs/', HQ_list.as_view()),
-    path('hqs/<int:id>', HQ_details.as_view()),
+    path('hqs/<int:id>/', GenericApiView.as_view()),
+    path('generic/hqs/', GenericApiView.as_view()),
+    path('generic/hqs/<int:id>/', GenericApiView.as_view()),
 ]
